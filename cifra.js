770cf335507bfcabbb1b7a83b4fa2b0cf6ed16f2
Pedro Victor <pedro20002victor@gmail.com>
2022-10-31 09:34:25 -0300

    msg descripto

--- cifra.js
+++ cifra.js
@@ -8,4 +8,14 @@ function criptMsg (msg, movimentos) {
    return msgCifrada.join('')
 }
 
-console.log(criptMsg(msgSecret, 3))
+const mensagemCifrada = criptMsg(msgSecret, 3)
+
+function decifraMsg (msg, movimentos) {
+   const msgCifrada = msg.split('').map(character => {
+      const codigoCaracter = character.charCodeAt(0);
+      return String.fromCharCode(codigoCaracter - movimentos)
+   })
+   return msgCifrada.join('')
+}
+
+console.log(mensagemCifrada, '\n', decifraMsg(mensagemCifrada, 3))
