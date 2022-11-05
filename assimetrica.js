eb8300e44ba61f881e287e01b9c6168c056247c6
Pedro Victor <pedro20002victor@gmail.com>
2022-11-05 13:26:13 -0300

    encriptacao assimetrica

---
+++ assimetrica.js
@@ -0,0 +1,16 @@
+import {generateKeyPairSync} from "crypto";
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+   modulusLength: 2048,
+   publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem'
+   },
+   privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem'
+   }
+});
+
+console.log(publicKey);
+console.log(privateKey)
